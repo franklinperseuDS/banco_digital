9d6c3ac9109ff2de859757b1fde308f42f49a43a
perseu-at-237228232680 <69854802+franklinperseuDS@users.noreply.github.com>
2022-11-20 08:54:46 -0400

    colocando funcoes e modificadores de acesso

--- dio.banco.digital/src/ContaPoupanca.java
+++ dio.banco.digital/src/ContaPoupanca.java
@@ -1,2 +1,10 @@
 public class ContaPoupanca {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar() {}
+    public void depositar() {}
+    public void transferir(){}
 }
